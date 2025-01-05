ef60eaf54cbef2b728633227bf2a97faee37ec76
Nathan Istace <nathan.istace@fishingcactus.com>
2025-01-05 16:17:00 +0100

    Updated to Unity 6000, some more playing around with hex grids

--- BossRushJam2025/Assets/Scripts/BossRushJam25/HexGridController.cs
+++ BossRushJam2025/Assets/Scripts/BossRushJam25/HexGridController.cs
@@ -1,9 +1,13 @@
+using System.Linq;
+using UnityEditor;
 using UnityEngine;
 
 namespace BossRushJam25 {
    public class HexGridController : MonoBehaviour {
       [SerializeField] protected Vector2Int gridSize = new(10, 10);
       [SerializeField] protected float hexRadius = 1;
+      [SerializeField] protected GameObject hexTilePrefab;
+      [SerializeField] protected Transform cursor;
 
       private float InnerRadius { get; set; }
 
@@ -13,10 +17,27 @@ namespace BossRushJam25 {
          RefreshInnerRadius();
       }
 
-      private Vector3 GetHexCenterAtCoordinates(Vector2Int coordinates) {
+      private Vector3 CoordinatesToWorldPosition(Vector2Int coordinates) {
          var x = coordinates.x;
          var z = coordinates.y;
          return new Vector3((2 * x + z % 2) * InnerRadius, 0, 1.5f * z * hexRadius);
+      }
+
+      private Vector2Int WorldToCoordinates(Vector3 worldPosition) {
+         var x = Mathf.FloorToInt((worldPosition.x / InnerRadius - .5f) / 2);
+         var y = Mathf.FloorToInt(worldPosition.z / hexRadius / 1.5f);
+
+         var surroundingHexesCoordinates = new[] { new Vector2Int(x, y), new Vector2Int(x + 1, y), new Vector2Int(x, y + 1), new Vector2Int(x + 1, y + 1) };
+
+         return surroundingHexesCoordinates.OrderBy(t => Vector3.SqrMagnitude(CoordinatesToWorldPosition(t) - worldPosition)).First();
+      }
+
+      private bool IsHexInGrid(Vector2Int coordinates) {
+         if (coordinates.x < 0) return false;
+         if (coordinates.y < 0) return false;
+         if (coordinates.x >= gridSize.x) return false;
+         if (coordinates.y >= gridSize.y) return false;
+         return true;
       }
 
       private void OnDrawGizmosSelected() {
@@ -26,7 +47,7 @@ namespace BossRushJam25 {
 
          for (var x = 0; x < gridSize.x; x++) {
             for (var z = 0; z < gridSize.y; z++) {
-               var hexCenter = GetHexCenterAtCoordinates(new Vector2Int(x, z));
+               var hexCenter = CoordinatesToWorldPosition(new Vector2Int(x, z));
                DrawGizmoHex(hexCenter);
             }
          }
@@ -40,5 +61,35 @@ namespace BossRushJam25 {
          }
          Gizmos.matrix = Matrix4x4.identity;
       }
+
+      private void OnDrawGizmos() {
+         if (!cursor) return;
+         var cursorWorldCoordinates = WorldToCoordinates(cursor.position);
+         var hexInGrid = IsHexInGrid(cursorWorldCoordinates);
+         Gizmos.color = Color.yellow * (hexInGrid ? 1 : .5f);
+         Gizmos.DrawCube(CoordinatesToWorldPosition(cursorWorldCoordinates), new Vector3(.5f, .5f, .5f));
+         Gizmos.color = Color.green * (hexInGrid ? 1 : .5f);
+         Gizmos.DrawCube(cursor.position, new Vector3(.1f, .7f, .1f));
+      }
+
+#if UNITY_EDITOR
+      [ContextMenu("Build")]
+      private void Build() {
+         if (Application.isPlaying) return;
+
+         while (transform.childCount > 0) {
+            DestroyImmediate(transform.GetChild(0).gameObject);
+         }
+
+         for (var x = 0; x < gridSize.x; x++) {
+            for (var z = 0; z < gridSize.y; z++) {
+               var hex = PrefabUtility.InstantiatePrefab(hexTilePrefab, transform) as GameObject;
+               hex.name = $"Hex_{x}_{z}";
+               hex.transform.position = CoordinatesToWorldPosition(new Vector2Int(x, z));
+               hex.transform.rotation = Quaternion.identity;
+            }
+         }
+      }
+#endif
    }
 }
