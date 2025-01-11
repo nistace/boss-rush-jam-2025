1d4fe1930b3cf03ddfcb25496330f2e43f807571
Théo Constant <to.constant@gmail.com>
2025-01-11 12:12:40 +0100

    Move scripts in right folder + add namespaces

---
+++ BossRushJam2025/Assets/Scripts/Character/DebugDestinationAssigner.cs
@@ -0,0 +1,30 @@
+using UnityEngine;
+using UnityEngine.AI;
+
+namespace BossRushJam25.Character
+{
+    public class DebugDestinationAssigner : MonoBehaviour
+    {
+        [SerializeField] protected NavMeshAgent navMeshAgent;
+
+        private void Update()
+        {
+            if(Input.GetMouseButtonDown(1))
+            {
+                if(Physics.Raycast(Camera.main.ScreenPointToRay(Input.mousePosition), out RaycastHit hit, 500) && hit.collider.gameObject.layer == LayerMask.NameToLayer("Ground"))
+                {
+                    navMeshAgent.destination = hit.point;
+                }
+            }
+        }
+
+        private void OnDrawGizmos()
+        {
+            if(navMeshAgent.hasPath)
+            {
+                Gizmos.color = Color.blue;
+                Gizmos.DrawSphere(navMeshAgent.destination, 0.3f);
+            }
+        }
+    }
+}
