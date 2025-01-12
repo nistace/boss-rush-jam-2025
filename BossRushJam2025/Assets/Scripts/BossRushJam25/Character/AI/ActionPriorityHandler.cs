5e54ef82d30da6336b594ad5a981357f99f35522
Théo Constant <to.constant@gmail.com>
2025-01-12 22:19:14 +0100

    Remove action queue autofill

--- BossRushJam2025/Assets/Scripts/BossRushJam25/Character/AI/ActionPriorityHandler.cs
+++ BossRushJam2025/Assets/Scripts/BossRushJam25/Character/AI/ActionPriorityHandler.cs
@@ -1,8 +1,6 @@
 using System.Collections.Generic;
 using System.Text;
 using BossRushJam25.Character.AI.Actions;
-using BossRushJam25.GameControllers;
-using BossRushJam25.HexGrid;
 using UnityEngine;
 using Utils;
 
@@ -84,27 +82,6 @@ namespace BossRushJam25.Character.AI
             }
         }
 
-        private void TryPlanNewAction()
-        {
-            if (plannedActions.Count >= queueSize)
-            {
-                return;
-            }
-
-            //TODO: use enum
-            int randomIndex = Random.Range(0,3);
-
-            APlannedAction action = randomIndex switch
-            {
-                0 => new MoveAction(character, HexGridController.Instance.GetRandomPositionOnNavMesh()),
-                1 => new CollectPowerUpAction(character, GameConfig.Instance.PowerUpsManager.powerUps[0]),
-                2 => new TakeCoverAction(character),
-                _ => throw new System.NotImplementedException()
-            };
-
-            PlanAction(action);
-        }
-
         private void DrawPreviews()
         {
             if(!drawPreviews)
@@ -127,7 +104,6 @@ namespace BossRushJam25.Character.AI
         private void Update()
         {
             ProcessActivePlannedAction();
-            TryPlanNewAction();
             DrawPreviews();
         }
 
