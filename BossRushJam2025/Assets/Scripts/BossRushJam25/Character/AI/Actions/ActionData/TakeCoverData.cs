906d826abd073b1a74e934b9b27b8bc41db9e15c
Théo Constant <to.constant@gmail.com>
2025-01-12 21:36:21 +0100

    Create takecover data

--- BossRushJam2025/Assets/Scripts/BossRushJam25/Character/AI/Actions/ActionData/TakeCoverData.cs
+++ BossRushJam2025/Assets/Scripts/BossRushJam25/Character/AI/Actions/ActionData/TakeCoverData.cs
@@ -3,5 +3,9 @@ using UnityEngine;
 [CreateAssetMenu(fileName = "TakeCoverData", menuName = "Actions/TakeCoverData")]
 public class TakeCoverData : AActionData
 {
+    [SerializeField] private float coverDetectionRadius = 10f;
+    [SerializeField] private float distanceWithCover = 3f;
 
+    public float CoverDetectionRadius => coverDetectionRadius;
+    public float DistanceWithCover => distanceWithCover;
 }
