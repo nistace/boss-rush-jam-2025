d01edf2325fee2a1da2dc4c8ff7504b30c1de985
nathan.istace <nathan.istace@fishingcactus.com>
2025-01-21 21:23:39 +0100

    Working on the character forward. It's broken I know. I'll fix it. Sorry guys. Love.

--- BossRushJam2025/Assets/Scripts/BossRushJam25/Common/AlignWithCameraForward.cs
+++ BossRushJam2025/Assets/Scripts/BossRushJam25/Common/AlignWithCameraForward.cs
@@ -1,11 +1,30 @@
+using System;
 using UnityEngine;
 
 public class AlignWithCameraForward : MonoBehaviour {
-   [SerializeField] protected bool keepVertical;
-   private void Update() {
-      var forward = Camera.main.transform.forward;
-      if (keepVertical) forward.y = 0;
 
-      transform.forward = forward;
+   public enum EStrategy {
+      Clamp = 0,
+      Remap = 1,
+   }
+
+   [SerializeField] protected bool keepVertical;
+
+   [SerializeField] protected EStrategy strategy;
+   [SerializeField] protected float minAngleWithForward = 30;
+   [SerializeField] protected float maxAngleWithForward = 60;
+
+   private void Update() {
+      var cameraForward = Camera.main.transform.forward;
+
+      var angleBetweenForwards = Vector3.Angle(cameraForward, Vector3.forward);
+
+      var selfAngleWithForward = strategy switch {
+         EStrategy.Clamp => Mathf.Clamp(angleBetweenForwards, minAngleWithForward, maxAngleWithForward),
+         EStrategy.Remap => minAngleWithForward + Mathf.InverseLerp(0, 90, angleBetweenForwards) * (maxAngleWithForward - minAngleWithForward),
+         _ => angleBetweenForwards
+      };
+
+      transform.forward = Vector3.Slerp(Vector3.forward, Vector3.down, selfAngleWithForward / 90);
    }
 }
