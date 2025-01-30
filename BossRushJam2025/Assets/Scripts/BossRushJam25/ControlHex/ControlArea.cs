2df6af17b11662135f5ac0c755c8f7d1ce2eeaad
Nathan Istace <nathan.istace@fishingcactus.com>
2025-01-30 22:04:01 +0100

    Fixed null refs and missing scripts

--- BossRushJam2025/Assets/Scripts/BossRushJam25/ControlHex/ControlArea.cs
+++ BossRushJam2025/Assets/Scripts/BossRushJam25/ControlHex/ControlArea.cs
@@ -1,32 +1,27 @@
+using BossRushJam25.ControlHex;
 using UnityEngine;
 
-public class ControlArea : MonoBehaviour
-{
-    [SerializeField] private float controlRadius;
-    [SerializeField] private MeshRenderer meshRenderer;
-    [SerializeField] private Color colorWhenHeroInside;
-    [SerializeField] private Color colorWhenHeroOutside;
+public class ControlArea : MonoBehaviour {
+   [SerializeField] protected HexContentDetector contentDetector;
+   [SerializeField] private MeshRenderer meshRenderer;
+   [SerializeField] private Color colorWhenHeroInside;
+   [SerializeField] private Color colorWhenHeroOutside;
 
-    public float ControlRadius => controlRadius;
+   public float ControlRadius => contentDetector.DetectionRadius;
 
-    private void OnTriggerEnter(Collider collider)
-    {
-        if(collider.gameObject.layer == LayerMask.NameToLayer("Hero"))
-        {
-            meshRenderer.material.SetColor("_Color", colorWhenHeroInside);
-        }
-    }
+   private void OnTriggerEnter(Collider collider) {
+      if (collider.gameObject.layer == LayerMask.NameToLayer("Hero")) {
+         meshRenderer.material.SetColor("_Color", colorWhenHeroInside);
+      }
+   }
 
-    private void OnTriggerExit(Collider collider)
-    {
-        if (collider.gameObject.layer == LayerMask.NameToLayer("Hero"))
-        {
-            meshRenderer.material.SetColor("_Color", colorWhenHeroOutside);
-        }
-    }
+   private void OnTriggerExit(Collider collider) {
+      if (collider.gameObject.layer == LayerMask.NameToLayer("Hero")) {
+         meshRenderer.material.SetColor("_Color", colorWhenHeroOutside);
+      }
+   }
 
-    private void OnValidate()
-    {
-        transform.localScale = new Vector3(ControlRadius * 2, 1f, ControlRadius * 2);
-    }
+   private void OnValidate() {
+      transform.localScale = new Vector3(ControlRadius * 2, 1f, ControlRadius * 2);
+   }
 }
--- BossRushJam2025/Assets/Scripts/BossRushJam25/ControlHex/HexContentDetector.cs
+++ BossRushJam2025/Assets/Scripts/BossRushJam25/ControlHex/HexContentDetector.cs
@@ -4,7 +4,7 @@ using BossRushJam25.HexGrid;
 using BossRushJam25.PowerUps;
 using UnityEngine;
 
-namespace BossRushJam25.Character {
+namespace BossRushJam25.ControlHex {
    public class HexContentDetector : MonoBehaviour {
       [SerializeField] protected float detectionRadius = 10f;
       [SerializeField] protected LayerMask detectionLayerMask = ~0;
@@ -12,10 +12,11 @@ namespace BossRushJam25.Character {
 
       private readonly Collider[] results = new Collider[50];
 
+      public float DetectionRadius => detectionRadius;
+
       public bool TryGetNearestDamageableHex(Vector3 fromPosition, DamageTypes damageTypes, out GridHex nearestHex) {
          var colliderCountInRange = Physics.OverlapSphereNonAlloc(transform.position, detectionRadius, results, detectionLayerMask, QueryTriggerInteraction.Collide);
-         return nearestHex = results
-            .Take(colliderCountInRange)
+         return nearestHex = results.Take(colliderCountInRange)
             .Select(t => t.gameObject.GetComponentInParent<GridHex>())
             .Where(t => t && t.ContentsAreDamageable(damageTypes))
             .OrderBy(t => (t.transform.position - fromPosition).sqrMagnitude)
@@ -24,9 +25,9 @@ namespace BossRushJam25.Character {
 
       public bool TryGetNearestPowerUp(Vector3 fromPosition, out PowerUp nearestPowerUp) {
          var colliderCountInRange = Physics.OverlapSphereNonAlloc(transform.position, detectionRadius, results, powerUpLayerMask, QueryTriggerInteraction.Collide);
-         return nearestPowerUp = results
-            .Take(colliderCountInRange)
+         return nearestPowerUp = results.Take(colliderCountInRange)
             .Select(t => t.gameObject.GetComponentInParent<PowerUp>())
+            .Where(t => t)
             .OrderBy(t => (t.transform.position - fromPosition).sqrMagnitude)
             .FirstOrDefault();
       }
