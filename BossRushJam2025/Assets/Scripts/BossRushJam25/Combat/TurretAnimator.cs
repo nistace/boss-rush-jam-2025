c3e99584cac419c05528f31674e87dded0dc82fd
Nathan Istace <nathan.istace@fishingcactus.com>
2025-01-30 23:17:40 +0100

    Turret model and animator

---
+++ BossRushJam2025/Assets/Scripts/BossRushJam25/Combat/TurretAnimator.cs
@@ -0,0 +1,22 @@
+﻿using UnityEngine;
+
+namespace BossRushJam25.Combat {
+   [RequireComponent(typeof(Animator))]
+   public class TurretAnimator : MonoBehaviour {
+      private static readonly int activeAnimParam = Animator.StringToHash("Active");
+      private static readonly int shootingAnimParam = Animator.StringToHash("Shooting");
+
+      [SerializeField] protected Animator animator;
+      [SerializeField] protected Turret turret;
+
+      private void Reset() {
+         animator = GetComponent<Animator>();
+         turret = GetComponentInParent<Turret>();
+      }
+
+      private void Update() {
+         animator.SetBool(activeAnimParam, turret.Active);
+         animator.SetBool(shootingAnimParam, turret.IsShooting);
+      }
+   }
+}
