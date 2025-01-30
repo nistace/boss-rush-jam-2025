7da8306316fa262c3dc14a3f16ba5ca7e5228d14
Nathan Istace <nathan.istace@fishingcactus.com>
2025-01-30 22:39:48 +0100

    Fixed priorities between actions. Hero always tries to go as close to the control hex as possible when there is no other better option

--- BossRushJam2025/Assets/Scripts/BossRushJam25/Character/AI/Actions/ActionTriggers/GoToControlHexWhenTooFar.cs
+++ BossRushJam2025/Assets/Scripts/BossRushJam25/Character/AI/Actions/ActionTriggers/GoToControlHexWhenTooFar.cs
@@ -1,31 +1,27 @@
 using BossRushJam25.HexGrid;
 using UnityEngine;
 
-namespace BossRushJam25.Character.AI.Actions.ActionTriggers
-{
-    [CreateAssetMenu(fileName = "GoToControlHexWhenTooFar", menuName = "ActionTriggers/GoToControlHexWhenTooFar")]
-    public class GoToControlHexWhenTooFar : AActionTrigger
-    {
-        [SerializeField] private float maxDistanceWithControlHex;
+namespace BossRushJam25.Character.AI.Actions.ActionTriggers {
+   [CreateAssetMenu(fileName = "GoToControlHexWhenTooFar", menuName = "ActionTriggers/GoToControlHexWhenTooFar")]
+   public class GoToControlHexWhenTooFar : AActionTrigger {
+      [SerializeField] private float maxDistanceWithControlHex;
 
-        public float MaxDistanceWithControlHex => maxDistanceWithControlHex;
+      public float MaxDistanceWithControlHex => maxDistanceWithControlHex;
 
-        public override bool TryGet(out AAction action)
-        {
-            action = null;
+      public override bool TryGet(out AAction action) {
+         action = null;
 
-            Vector3 controlHexPosition = HexGridController.Instance.ControlHex.transform.position;
+         Vector3 controlHexPosition = HexGridController.Instance.ControlHex.transform.position;
 
-            float sqrDistanceWithControlHex = (controlHexPosition - character.transform.position).sqrMagnitude;
+         float sqrDistanceWithControlHex = (controlHexPosition - character.transform.position).sqrMagnitude;
 
-            if(sqrDistanceWithControlHex < maxDistanceWithControlHex * maxDistanceWithControlHex)
-            {
-                return false;
-            }
+         if (sqrDistanceWithControlHex < maxDistanceWithControlHex * maxDistanceWithControlHex) {
+            return false;
+         }
 
-            action = new MoveAction(character, controlHexPosition, priority, distanceImpactsPriority: false);
+         action = new MoveAction(character, controlHexPosition, priority, distanceImpactsPriority: false);
 
-            return true;
-        }
-    }
+         return true;
+      }
+   }
 }
--- BossRushJam2025/Assets/Scripts/BossRushJam25/ControlHex/ControlArea.cs
+++ BossRushJam2025/Assets/Scripts/BossRushJam25/ControlHex/ControlArea.cs
@@ -1,27 +1,28 @@
-using BossRushJam25.ControlHex;
 using UnityEngine;
 
-public class ControlArea : MonoBehaviour {
-   [SerializeField] protected HexContentDetector contentDetector;
-   [SerializeField] private MeshRenderer meshRenderer;
-   [SerializeField] private Color colorWhenHeroInside;
-   [SerializeField] private Color colorWhenHeroOutside;
+namespace BossRushJam25.ControlHex {
+   public class ControlArea : MonoBehaviour {
+      [SerializeField] protected HexContentDetector contentDetector;
+      [SerializeField] private MeshRenderer meshRenderer;
+      [SerializeField] private Color colorWhenHeroInside;
+      [SerializeField] private Color colorWhenHeroOutside;
 
-   public float ControlRadius => contentDetector.DetectionRadius;
+      public float ControlRadius => contentDetector.DetectionRadius;
 
-   private void OnTriggerEnter(Collider collider) {
-      if (collider.gameObject.layer == LayerMask.NameToLayer("Hero")) {
-         meshRenderer.material.SetColor("_Color", colorWhenHeroInside);
+      private void OnTriggerEnter(Collider collider) {
+         if (collider.gameObject.layer == LayerMask.NameToLayer("Hero")) {
+            meshRenderer.material.SetColor("_Color", colorWhenHeroInside);
+         }
       }
-   }
 
-   private void OnTriggerExit(Collider collider) {
-      if (collider.gameObject.layer == LayerMask.NameToLayer("Hero")) {
-         meshRenderer.material.SetColor("_Color", colorWhenHeroOutside);
+      private void OnTriggerExit(Collider collider) {
+         if (collider.gameObject.layer == LayerMask.NameToLayer("Hero")) {
+            meshRenderer.material.SetColor("_Color", colorWhenHeroOutside);
+         }
       }
-   }
 
-   private void OnValidate() {
-      transform.localScale = new Vector3(ControlRadius * 2, 1f, ControlRadius * 2);
+      private void OnValidate() {
+         transform.localScale = new Vector3(ControlRadius * 2, 1f, ControlRadius * 2);
+      }
    }
 }
