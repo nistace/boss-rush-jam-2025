cc4ab8047e14f8332bcadc49924bb9a0c93309bb
Nathan Istace <nathan.istace@fishingcactus.com>
2025-02-01 01:34:57 +0100

    Camera positions are handled by a transform instead of just a position. Moved menu camera

--- BossRushJam2025/Assets/Scripts/BossRushJam25/Cameras/CameraController.cs
+++ BossRushJam2025/Assets/Scripts/BossRushJam25/Cameras/CameraController.cs
@@ -7,8 +7,8 @@ namespace BossRushJam25.Cameras {
    public class CameraController : MonoBehaviour {
       public static CameraController Instance { get; private set; }
 
-      [SerializeField] protected Vector3 menuPosition;
-      [SerializeField] protected Vector3 gamePosition;
+      [SerializeField] protected Transform gameCameraPositioning;
+      [SerializeField] protected Transform menuCameraPositioning;
       [SerializeField] protected float smoothVelocity;
       [SerializeField] protected float maxSpeed;
       [SerializeField] protected float minPitch;
@@ -18,40 +18,45 @@ namespace BossRushJam25.Cameras {
       protected float farthestGridZPosition;
       protected float closestGridZPosition;
 
-      private Vector3 CurrentTargetPosition { get; set; }
-      public float SqrDistanceWithTargetPosition => (transform.position - CurrentTargetPosition).sqrMagnitude;
+      private Transform CurrentTargetPosition { get; set; }
+      public float SqrDistanceWithTargetPosition => CurrentTargetPosition ? (transform.position - CurrentTargetPosition.position).sqrMagnitude : 0;
 
       private Vector3 currentVelocity;
+      private Vector3 currentRotationVelocity;
       private float pitchCurrentVelocity;
 
       private void Awake() {
          Instance = this;
       }
 
-      private void Start()
-      {
+      private void Start() {
          farthestGridZPosition = HexGridController.Instance.CoordinatesToWorldPosition(new(0, HexGridController.Instance.GridRadius)).z;
          closestGridZPosition = -farthestGridZPosition;
       }
 
       private void Update() {
-         transform.position = Vector3.SmoothDamp(transform.position, CurrentTargetPosition, ref currentVelocity, smoothVelocity, maxSpeed);
+         if (CurrentTargetPosition) {
+            transform.position = Vector3.SmoothDamp(transform.position, CurrentTargetPosition ? CurrentTargetPosition.position : transform.position, ref currentVelocity, smoothVelocity, maxSpeed);
+            transform.forward = Vector3.SmoothDamp(transform.forward, CurrentTargetPosition ? CurrentTargetPosition.forward : transform.forward, ref currentRotationVelocity, smoothVelocity, maxSpeed);
+         }
 
-         if(BossFightInfo.Hero != null)
-         {
+         if (BossFightInfo.Hero != null) {
             float targetedPitch = math.remap(farthestGridZPosition, closestGridZPosition, maxPitch, minPitch, BossFightInfo.Hero.transform.position.z);
-            cameraRotation.localEulerAngles = new(Mathf.SmoothDampAngle(cameraRotation.localEulerAngles.x, targetedPitch, ref pitchCurrentVelocity, smoothVelocity), cameraRotation.localEulerAngles.y, cameraRotation.localEulerAngles.z);
+            cameraRotation.localEulerAngles = new(Mathf.SmoothDampAngle(cameraRotation.localEulerAngles.x, targetedPitch, ref pitchCurrentVelocity, smoothVelocity),
+               cameraRotation.localEulerAngles.y,
+               cameraRotation.localEulerAngles.z);
          }
       }
 
-      public void MoveToMenuPosition(bool snap) => MoveToPosition(menuPosition, snap);
-      public void MoveToGamePosition(bool snap) => MoveToPosition(gamePosition, snap);
+      public void MoveToMenuPosition(bool snap) => MoveToPosition(menuCameraPositioning, snap);
+      public void MoveToGamePosition(bool snap) => MoveToPosition(gameCameraPositioning, snap);
 
-      private void MoveToPosition(Vector3 position, bool snap) {
-         CurrentTargetPosition = position;
+      private void MoveToPosition(Transform positioning, bool snap) {
+         CurrentTargetPosition = positioning;
 
-         if (snap) {
-            transform.position = CurrentTargetPosition;
+         if (CurrentTargetPosition && snap) {
+            transform.position = CurrentTargetPosition.position;
+            transform.rotation = CurrentTargetPosition.rotation;
             currentVelocity = Vector3.zero;
          }
       }
